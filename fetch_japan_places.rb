882af65394aa74d39c5869b9ddee9c7536e94417
murano-masaki <murano-masaki@kayac.com>
2016-07-05 08:44:17 +0900

    get japanese. format yaml.

--- fetch_japan_places.rb
+++ fetch_japan_places.rb
@@ -3,17 +3,30 @@ require 'json'
 require 'yaml'
 require 'pp'
 
+def place_to_yml(place)
+  yml = {}
+  yml[place.woeid] = {
+    name: place.name,
+    type: place.placetype
+  }
+  yml
+end
+
 GeoPlanet.appid = ENV['APPID']
 GeoPlanet.debug = true
 
-japan = GeoPlanet::Place.search("japan", type:12).first
-region = japan.children(type: "Colloquial", count:0).select! {|r| r.name.index('Region')}
+geo_yml = {}
 
-pp region.to_yaml
-p region.length
+japan = GeoPlanet::Place.search("japan", type:12, lang:'ja').first
+geo_yml.merge!(place_to_yml(japan))
 
+region = japan.children(type: "Colloquial", count:0, lang:'ja').select! {|r| r.name.index('地方')}
+# japan.children has many other type of places.
+# Colloquial includes peninsula somehow.
 region.each do |r|
-  p r.children(count:0)
+  geo_yml.merge!(place_to_yml(r))
+  # p r.children(count:0)
 end
 
-File.write(Dir.pwd + "/geoplanet.yml", region.to_yaml)
+File.write(Dir.pwd + "/geoplanet.yml", geo_yml.to_yaml)
+pp geo_yml
