98c2ad285d9c4f348cff51676dd0ffaafc7011e8
murano-masaki <murano-masaki@kayac.com>
2016-07-05 23:07:34 +0900

    fetch all children.

--- fetch_japan_places.rb
+++ fetch_japan_places.rb
@@ -3,30 +3,39 @@ require 'json'
 require 'yaml'
 require 'pp'
 
-def place_to_yml(place)
-  yml = {}
-  yml[place.woeid] = {
-    name: place.name,
-    type: place.placetype
+JAPAN_WOEID = 23424856
+$place_num = 0
+
+def place_to_yml(place, parent, children)
+  {
+    place.woeid => {
+      type: place.placetype,
+      name: place.name,
+      parent: parent ? parent.woeid : nil,
+      children: children ? children.map {|c| c.woeid} : nil
+    }
   }
+end
+
+def get_children_tree(place, parent)
+  yml = {}
+  $place_num += 1
+  puts "fetching : " + place.name
+  puts "place num : " + $place_num
+  children = place.children(type: [7, 8, 9, 10], count:0, lang:'ja')
+  yml.merge!(place_to_yml(place, parent, children))
+  if children
+    children.each do |c|
+      yml.merge!(get_children_tree(c, place))
+    end
+  end
   yml
 end
 
 GeoPlanet.appid = ENV['APPID']
 GeoPlanet.debug = true
 
-geo_yml = {}
-
-japan = GeoPlanet::Place.search("japan", type:12, lang:'ja').first
-geo_yml.merge!(place_to_yml(japan))
-
-region = japan.children(type: "Colloquial", count:0, lang:'ja').select! {|r| r.name.index('地方')}
-# japan.children has many other type of places.
-# Colloquial includes peninsula somehow.
-region.each do |r|
-  geo_yml.merge!(place_to_yml(r))
-  # p r.children(count:0)
-end
+japan = GeoPlanet::Place.new(JAPAN_WOEID, lang:'ja')
+geo_yml = get_children_tree(japan, nil)
 
 File.write(Dir.pwd + "/geoplanet.yml", geo_yml.to_yaml)
-pp geo_yml
