9ef2a382c861b2ed4693857c8d98ac97d1a80acb
murano-masaki <murano-masaki@kayac.com>
2016-07-08 12:49:12 +0900

    add tree format.

--- fetch_japan_places.rb
+++ fetch_japan_places.rb
@@ -17,12 +17,20 @@ def place_to_yml(place, parent, children)
   }
 end
 
+def place_to_yml_as_tree(place)
+  {
+    place.woeid => {
+      type: place.placetype,
+      name: place.name
+    }
+  }
+end
+
 def get_children_tree(place, parent)
   $place_num += 1
   puts "fetching : " + place.name + ", " + place.placetype
   puts "place num : " + $place_num.to_s
-  children = place.children(type: [7, 8, 9, 10], count:0, lang:'ja')
-  # children = place.children(type: [7, 8, 9], count:0, lang:'ja')
+  children = place.children(type: [7, 8, 9], count:0, lang:'ja')
   yml = place_to_yml(place, parent, children)
   if children
     children.each do |c|
@@ -32,10 +40,27 @@ def get_children_tree(place, parent)
   return yml
 end
 
+def get_children_tree_as_tree_hash(place, _hash)
+  $place_num += 1
+  puts "fetching : " + place.name + ", " + place.placetype
+  puts "place num : " + $place_num.to_s
+  _hash.merge!(place_to_yml_as_tree(place))
+
+  children = place.children(type: [7, 8], count:0, lang:'ja')
+  if children
+    children.each do |c|
+      get_children_tree_as_tree_hash(c, _hash[place.woeid])
+    end
+  end
+end
+
 GeoPlanet.appid = ENV['APPID']
 GeoPlanet.debug = true
 
 japan = GeoPlanet::Place.new(JAPAN_WOEID, lang:'ja')
-geo_yml = get_children_tree(japan, nil)
+# geo_yml = get_children_tree(japan, nil)
+geo_yml = {}
+get_children_tree_as_tree_hash(japan, geo_yml)
 
-File.write(Dir.pwd + "/geoplanet.yml", geo_yml.to_yaml)
+# File.write(Dir.pwd + "/geoplanet.yml", geo_yml.to_yaml)
+File.write(Dir.pwd + "/geoplanet_tree.yml", geo_yml.to_yaml)
