6e98351d1d7139d3f9cdc9a008ded45363da5d62
murano-masaki <murano-masaki@kayac.com>
2016-07-13 08:35:43 +0900

    fix https://github.com/Samemura/my-geoplanet/issues/1#issuecomment-231572340

--- fetch_geoplanet_place.rb
+++ fetch_geoplanet_place.rb
@@ -2,10 +2,17 @@ require 'geoplanet'
 require 'yaml'
 require 'pry'
 
-place_woeid = (ARGV[0] ? ARGV[0].to_i : 23424856)
-file_name = (ARGV[1] ? ARGV[1] : "geoplanet.yml")
+JAPAN_WOEID = 23424856
+PLACE_LANGUAGE = 'ja'
+PLACE_COUNT = 0
+PLACE_TYPE = [8]
+PLACE_SELECT = 'long'
 
-def place_to_yml(place, parent)
+place_woeid = (ARGV[0] ? ARGV[0].to_i : JAPAN_WOEID)
+file_name = (ARGV[1] ? ARGV[1] : "geoplanet.yml")
+debug_mode = (ARGV[2] ? ARGV[2] : false)
+
+def place_to_hahs(place, parent)
   {
     place.woeid => {
       type: place.placetype,
@@ -18,6 +25,7 @@ def place_to_yml(place, parent)
       admin3: place.admin3,
       uri: place.uri,
       latitude: place.latitude,
+      longitude: place.longitude,
       bounding_box: place.bounding_box,
       area_rank: place.area_rank,
       parent: parent ? parent.woeid : nil,
@@ -30,21 +38,18 @@ def get_children_tree(place, parent, _array, _tree)
   $place_num += 1
   puts "fetching (" + $place_num.to_s + "): " + place.name + ", " + place.placetype
 
-  children = place.children(select: 'long', type: [7, 8, 9], count:0, lang:'ja')
-
-  hash = place_to_yml(place, parent)
+  hash = place_to_hahs(place, parent)
   _array.merge!(hash)
   _tree.merge!(hash)
 
-  if children
-    children.each do |c|
-      get_children_tree(c, place, _array, _tree[place.woeid][:children])
-    end
+  children = place.children(select: PLACE_SELECT, type: PLACE_TYPE, count: PLACE_COUNT, lang: PLACE_LANGUAGE) || []
+  children.each do |c|
+    get_children_tree(c, place, _array, _tree[place.woeid][:children])
   end
 end
 
 GeoPlanet.appid = ENV['GEOPLANET_APPID']
-# GeoPlanet.debug = true
+GeoPlanet.debug = debug_mode
 
 place = GeoPlanet::Place.new(place_woeid, lang:'ja')
 
@@ -55,4 +60,5 @@ get_children_tree(place, nil, array_hash, tree_hash)
 
 file_path = Dir.pwd + "/" + file_name
 File.write(file_path, array_hash.to_yaml)
+
 puts file_path + " successfully generated."
