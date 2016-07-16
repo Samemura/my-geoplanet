0a11198dae9fa01f4357c8aaa3ab0e1550fcb11e
murano-masaki <murano-masaki@kayac.com>
2016-07-16 15:48:09 +0900

    move monkey patch to gem.

--- fetch_geoplanet_place.rb
+++ fetch_geoplanet_place.rb
@@ -17,53 +17,41 @@ place_woeid = (ARGV[0] || JAPAN_WOEID).to_i
 file_name = (ARGV[1] || "geoplanet.yml")
 debug_mode = (ARGV[2] || false)
 
-# method
-class GeoPlanet::Place
-  def self.get_then_parse(url)
-    results = JSON.parse get(url).to_s
-    return results['places']['place'].map{|attrs| self.new attrs} if results['places']
-    return self.new(results['place']) if results['place']
-    nil
-  rescue
-    nil
-  end
-
-  def children(options = {})
-    self.class.send("children_of", self.woeid, options)
-  end
-end
-
 class MyPlace < GeoPlanet::Place
   attr_accessor :parent, :chidlren
 
   def to_h(parent)
-    binding.pry
-    hash = {
-      self.woeid => {
-        parent: parent ? parent.woeid : nil,
-        children: {}
-      }
+    {
+      self.woeid => Hash[instance_variables.map {|i| [i.to_s.sub("@", ""), instance_variable_get(i)] }]
     }
-    hash[self.woeid].merge! Hash[instance_variables.map {|i| [i.to_s.sub("@", ""), instance_variable_get(i)] }]
-    return hash
+    # hash[self.woeid].merge! Hash[instance_variables.map {|i| [i.to_s.sub("@", ""), instance_variable_get(i)] }]
+    # return hash
   end
 
   def children(*args)
     p "do children"
-    @children ||= super(*args).map {|c| c.parent = self; return c}
+    # @children ||= super(*args).map {|c| c.parent = self.woeid}
+    children = super(*args)
+    @children = children.map {|c| [c.woeid, c] }.to_h
+    return children
   end
 end
 
 def get_children_tree(place, parent, _array, _tree)
   yield place if block_given?
 
+  place.children
+  place.parent = parent ? parent.woeid : nil
   hash = place.to_h(parent)
+
+  binding.pry
+
   _array.merge!(hash)
   _tree.merge!(hash)
 
   children = place.children(PLACE_ATTR) || []
   children.each do |c|
-    get_children_tree(c, place, _array, _tree[place.woeid][:children]) {|place| yield place if block_given?}
+    get_children_tree(c, place, _array, _tree[place.woeid]["children"]) {|place| yield place if block_given?}
   end
 end
 
